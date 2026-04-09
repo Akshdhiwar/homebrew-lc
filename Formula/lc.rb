be34a7383f9e5923cd36ba9c69856ef245079936
Akash Dhiwar <akashdhiwar3001@gmail.com>
2026-04-09 22:32:06 +0530

    Remove 'sha256:' prefix from checksum values

--- Formula/lc.rb
+++ Formula/lc.rb
@@ -5,10 +5,10 @@ class Lc < Formula
 
   if Hardware::CPU.arm?
     url "https://github.com/Akshdhiwar/leetcode-terminal/releases/download/latest/lc-darwin-arm64"
-    sha256 "sha256:cb6e4f1c7a7e26c1047c8746dc93c02819f5a3c40f76874c677b9c37f7a3f8ee"
+    sha256 "cb6e4f1c7a7e26c1047c8746dc93c02819f5a3c40f76874c677b9c37f7a3f8ee"
   else
     url "https://github.com/Akshdhiwar/leetcode-terminal/releases/download/latest/lc-darwin-amd64"
-    sha256 "sha256:198d7437f56b14fb8194e254faf45c5d896fa8b91d2f217b1a5932b831be8725"
+    sha256 "198d7437f56b14fb8194e254faf45c5d896fa8b91d2f217b1a5932b831be8725"
   end
 
   def install
