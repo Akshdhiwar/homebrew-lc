3173beab2637830c1c100f49d058c99ab6ada14c
Akash Dhiwar <akashdhiwar3001@gmail.com>
2026-04-09 22:21:05 +0530

    Update lc.rb to use latest release URLs

--- Formula/lc.rb
+++ Formula/lc.rb
@@ -4,10 +4,10 @@ class Lc < Formula
   version "1.0.1"
 
   if Hardware::CPU.arm?
-    url "https://github.com/Akshdhiwar/leetcode-terminal/releases/tag/v1.0.1/lc-darwin-arm64"
+    url "https://github.com/Akshdhiwar/leetcode-terminal/releases/tag/latest/lc-darwin-arm64"
     sha256 "sha256:cb6e4f1c7a7e26c1047c8746dc93c02819f5a3c40f76874c677b9c37f7a3f8ee"
   else
-    url "https://github.com/Akshdhiwar/leetcode-terminal/releases/tag/v1.0.1/lc-darwin-amd64"
+    url "https://github.com/Akshdhiwar/leetcode-terminal/releases/tag/latest/lc-darwin-amd64"
     sha256 "sha256:198d7437f56b14fb8194e254faf45c5d896fa8b91d2f217b1a5932b831be8725"
   end
 
